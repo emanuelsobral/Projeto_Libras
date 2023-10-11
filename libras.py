9cd9192f42b64260b5aa1c278d35c62c36a2d2e7
emanuel <emanuelssobral@gmail.com>
2023-10-11 00:07:37 -0300

    alt codigo

--- libras.py
+++ libras.py
@@ -1,20 +1,29 @@
 import cv2
 import mediapipe as mp
 
-webcam = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
-while True:
-    success, img = webcam.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+cap = cv2.VideoCapture(0)
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands: 
+    while cap.isOpened():
+        ret, frame = cap.read()
         
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        results = hands.process(image)
+        
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        
+        cv2.imshow('MediaPipe Hands', image)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
